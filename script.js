783fb6c9b8309c1a326079653a0aefa3e2d3619c
User <gabrielkleber006@gmail.com>
2021-07-21 22:25:20 -0300

    requisitos 9,10,11,12.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const btnLogin = document.querySelector("#btn-login")
-const loginEmail = document.querySelector("#loginEmail")
-const loginSenha = document.querySelector("#loginSenha")
+const btnLogin = document.querySelector('#btn-login');
+const loginEmail = document.querySelector('#loginEmail');
+const loginSenha = document.querySelector('#loginSenha');
 
-btnLogin.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (loginEmail.value === "tryber@teste.com" && loginSenha.value === "123456") {
-        alert("Olá, Tryber!")
-    } else {
-        alert("Login ou senha inválidos.")
-    }
-})
+btnLogin.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (loginEmail.value === 'tryber@teste.com' && loginSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+});
