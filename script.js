42baa4f74800b441b422c70bf60df0f134af3208
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-24 12:30:57 -0300

    Requerimento 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const loginEmail = document.querySelector('#loginEmail');
 const loginSenha = document.querySelector('#loginSenha');
 const checkBox = document.querySelector('#agreement');
 const btnEnviar = document.querySelector('#submit-btn');
+const texto = document.getElementById('textarea');
+const count = document.getElementById('counter');
 btnLogin.addEventListener('click', (event) => {
   event.preventDefault();
   if (loginEmail.value === 'tryber@teste.com' && loginSenha.value === '123456') {
@@ -20,4 +22,8 @@ checkBox.addEventListener('change', (event) => {
   } else {
     btnEnviar.disabled = true;
   }
+});
+
+texto.addEventListener('keyup', () => {
+  count.innerText = texto.maxLength - texto.value.length;
 });
